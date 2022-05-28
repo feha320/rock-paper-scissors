fd8ea68c216f772f05a205cafe3369c0b5b4c961
Felix Hartung <f.hartung@posteo.de>
2022-05-28 13:53:57 +0200

    added 1-round gamemode: playRound()

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const rock = "Stein";
 const paper = "Papier";
 const scissors = "Schere";
-let computerChoice;
 
-function computerPlay () {
+function computerChoice () {
   randomChoice = Math.floor(Math.random() * 3) + 1;
   if (randomChoice == 1) {
     computerChoice = rock;
@@ -15,7 +14,44 @@ function computerPlay () {
   return computerChoice;
 }
 
-console.log(computerPlay());
+function playerChoice() {
+  let choice
+  input = prompt("1 für Schere, 2 für Stein, 3 für Papier")
+  if (input == "1") {
+    choice = scissors;
+  } else if (input == 2) {
+    choice = rock;
+  } else if (input == 3) {
+    choice = paper;
+  }
+  return choice;
+}
+
+function playRound(playerSelection, computerSelection) {
+  console.log("Spieler: " + playerSelection);
+  console.log("Computer: " + computerSelection);
+  if (playerSelection == rock && computerSelection == paper) {
+    console.log("Du verlierst!");
+  } else if (playerSelection == rock && computerSelection == scissors) {
+    console.log("Du gewinnst!");
+  } else if (playerSelection == paper && computerSelection == rock) {
+    console.log("Du gewinnst!");
+  } else if (playerSelection == paper && computerSelection == scissors) {
+    console.log("Du verlierst!");
+  }  else if (playerSelection == scissors && computerSelection == paper) {
+    console.log("Du gewinnst!");
+  } else if (playerSelection == scissors && computerSelection == rock) {
+    console.log("Du verlierst!");
+  } else if (playerSelection == computerSelection) {
+    console.log("Unentschieden!");
+  }
+}
+
+
+playRound(playerChoice(), computerChoice());
+
+
+
 
 
 
