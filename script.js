5981e239c1a6db802c486217c4f3c9e38264d7b8
Felix Hartung <f.hartung@posteo.de>
2022-08-20 14:37:29 +0200

    finished script.js and startet new UI

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ const cChoice = document.getElementById("computer-choice");
 
 const choices = ["rock", "paper", "scissors"];
 
+const yourPoints = document.getElementById("yourPoints");
+const computerPoints =  document.getElementById("computerPoints");
+
+const round = document.getElementById("round");
+
+let yourScore = 0;
+let computerScore = 0;
+
 scissors.addEventListener("click", function () {
   game("scissors");
   scissors.classList.remove("my-choice");
@@ -58,16 +66,23 @@ function game(choice) {
   if (pC + cC == "paperrock" ||
     pC + cC == "scissorspaper" ||
     pC + cC == "rockscissors") {
+      round.textContent = "DU GEWINNST!";
+      yourScore ++;
+      yourPoints.textContent = yourScore;
     console.log(pC + cC);
     console.log("WIN")
   } else if (pC + cC == "rockpaper" ||
     pC + cC == "paperscissors" ||
     pC + cC == "scissorsrock") {
+      round.textContent = "DU VERLIERST!";
+      computerScore ++;
+      computerPoints.textContent = computerScore;
     console.log(pC + cC);
     console.log("LOSE");
   } else if (pC + cC == "rockrock" ||
     pC + cC == "paperpaper" ||
     pC + cC == "scissorsscissors") {
+      round.textContent = "UNENTSCHIEDEN!";
     console.log(pC + cC);
     console.log("unentschieden");
   }
